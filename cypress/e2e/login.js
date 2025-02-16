a3e70a490bf3c30d180c4f9e2f325c00b0d3dee6
Pedro Thomé <pedro.th.dev@gmail.com>
2025-02-16 20:23:21 -0300

    add project

---
+++ cypress/e2e/login.js
@@ -0,0 +1,18 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("que estou na página de login", () => {
+  cy.visit("https://pedrothh.github.io/my-public-front-for-tests");
+});
+
+When("eu insiro um nome de usuário válido e senha válida", () => {
+  cy.get("#username").type("user");
+  cy.get("#password").type("12345");
+});
+
+When("clico no botão Entrar", () => {
+  cy.get('button[type="submit"]').click();
+});
+
+Then("eu devo ser redirecionado para a página inicial", () => {
+  cy.url().should("include", "/home");
+});
